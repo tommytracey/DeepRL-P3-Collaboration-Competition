04266fbd8d38d3b25f5ff2ca34bcf51d60105a8f
tommytracey <tommytracey@gmail.com>
2018-12-13 21:59:34 -0800

    added different approach to batch normalization

--- model.py
+++ model.py
@@ -12,7 +12,37 @@ def hidden_init(layer):
 class Actor(nn.Module):
     """Actor (Policy) Model."""
 
-    def __init__(self, state_size, action_size, seed, fc1_units=400, fc2_units=300):
+    # def __init__(self, state_size, action_size, seed, fc1_units=400, fc2_units=300):
+    #     """Initialize parameters and build model.
+    #     Params
+    #     ======
+    #         state_size (int): Dimension of each state
+    #         action_size (int): Dimension of each action
+    #         seed (int): Random seed
+    #         fc1_units (int): Number of nodes in first hidden layer
+    #         fc2_units (int): Number of nodes in second hidden layer
+    #     """
+    #     super(Actor, self).__init__()
+    #     self.seed = torch.manual_seed(seed)
+    #
+    #     self.fc1 = nn.Linear(state_size, fc1_units)
+    #     self.bn1 = nn.BatchNorm1d(fc1_units)
+    #     self.fc2 = nn.Linear(fc1_units, fc2_units)
+    #     self.fc3 = nn.Linear(fc2_units, action_size)
+    #     self.reset_parameters()
+    #
+    # def reset_parameters(self):
+    #     self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
+    #     self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
+    #     self.fc3.weight.data.uniform_(-3e-3, 3e-3)
+    #
+    # def forward(self, state):
+    #     """Build an actor (policy) network that maps states -> actions."""
+    #     x = F.relu(self.bn1(self.fc1(state)))
+    #     x = F.relu(self.fc2(x))
+    #     return F.tanh(self.fc3(x))
+
+    def __init__(self, state_size, action_size, seed, fc1_units=400, fc2_units=300, use_bn=False):
         """Initialize parameters and build model.
         Params
         ======
@@ -24,24 +54,40 @@ class Actor(nn.Module):
         """
         super(Actor, self).__init__()
         self.seed = torch.manual_seed(seed)
-
+        self.use_bn = use_bn
         self.fc1 = nn.Linear(state_size, fc1_units)
-        self.bn1 = nn.BatchNorm1d(fc1_units)
         self.fc2 = nn.Linear(fc1_units, fc2_units)
         self.fc3 = nn.Linear(fc2_units, action_size)
+        if self.use_bn:
+            self.bn1 = nn.BatchNorm1d(state_size)
+            self.bn2 = nn.BatchNorm1d(fc1_units)
+            self.bn3 = nn.BatchNorm1d(fc2_units)
+
         self.reset_parameters()
 
     def reset_parameters(self):
         self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
         self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
         self.fc3.weight.data.uniform_(-3e-3, 3e-3)
+        self.fc1.bias.data.fill_(0.1)
+        self.fc2.bias.data.fill_(0.1)
+        self.fc3.bias.data.fill_(0.1)
 
     def forward(self, state):
         """Build an actor (policy) network that maps states -> actions."""
-        x = F.relu(self.bn1(self.fc1(state)))
-        x = F.relu(self.fc2(x))
-        return F.tanh(self.fc3(x))
+        if self.use_bn:
+            x = self.fc1(self.bn1(state))
+        else:
+            x = self.fc1(state)
 
+        x = F.relu(x)
+        if self.use_bn:
+            x = self.bn2(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+        if self.use_bn:
+            x = self.bn3(x)
+        return F.tanh(self.fc3(x))
 
 class Critic(nn.Module):
     """Critic (Value) Model."""
