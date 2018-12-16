cf30d48b7ef48b0895cfee9ab8ed156734fcb06f
tommytracey <tommytracey@gmail.com>
2018-12-15 20:36:27 -0800

    first working version, solved in 541 episodes

--- main.py
+++ main.py
@@ -2,20 +2,20 @@
 # and performs training loop
 
 from collections import deque
-from maddpg import Agent, AgentTrainer
+from maddpg_agent import Agent #, AgentTrainer
 import numpy as np
 import os
 import time
 import torch
 from unityagents import UnityEnvironment
 
-N_EPISODES = 3000
-MAX_T = 1000
+N_EPISODES = 2000
 SOLVED_SCORE = 0.5
 CONSEC_EPISODES = 100
-PRINT_EVERY = 1
+PRINT_EVERY = 10
 ADD_NOISE = True
-MODEL_DIR = 'models/'
+# MODEL_DIR = 'models/'
+
 
 def seeding(seed=1):
     np.random.seed(seed)
@@ -49,76 +49,58 @@ def main():
     print('The state for the first agent looks like:', states[0])
 
     # initialize agents
-    # agent = AgentTrainer()
-    agent_0 = Agent(state_size, action_size, num_agents=1, random_seed=1)
-    agent_1 = Agent(state_size, action_size, num_agents=1, random_seed=1)
+    agent_0 = Agent(state_size, action_size, 1, random_seed=0)
+    agent_1 = Agent(state_size, action_size, 1, random_seed=0)
 
-    # scoring placeholders
-    mean_scores = []                               # list of mean scores from each episode
-    min_scores = []                                # list of lowest scores from each episode
-    max_scores = []                                # list of highest scores from each episode
-    best_score = -np.inf
-    scores_window = deque(maxlen=CONSEC_EPISODES)  # mean scores from most recent episodes
-    moving_avgs = []                               # list of moving averages
+    # n_episodes = 1000
+    scores_window = deque(maxlen=CONSEC_EPISODES)
+    scores_all = []
+    moving_average = []
 
-    # training loop
-    for i_episode in range(1, n_episodes+1):
-        start_time = time.time()
-        env_info = env.reset(train_mode=train_mode)[brain_name]    # reset environment
-        states = np.reshape(env_info.vector_observations, (1,48))  # get current state for each agent
-        scores = np.zeros(num_agents)                              # initialize score for each agent
-        # agent.reset()
+    for i_episode in range(1, N_EPISODES+1):
+        env_info = env.reset(train_mode=True)[brain_name]      # reset the environment
+        states = env_info.vector_observations
+        states = np.reshape(states, (1,48))
         agent_0.reset()
         agent_1.reset()
+        scores = np.zeros(num_agents)
+        while True:
+            action_0 = agent_0.act(states, ADD_NOISE)           # agent 1 chooses an action
+            action_1 = agent_1.act(states, ADD_NOISE)           # agent 2 chooses an action
+            actions = np.concatenate((action_0, action_1), axis=0).flatten()
+            env_info = env.step(actions)[brain_name]           # send both agents' actions together to the environment
+            next_states = env_info.vector_observations         # get next states
+            next_states = np.reshape(next_states, (1, 48))     # combine each agent's state into one state space
+            rewards = env_info.rewards                         # get reward
+            done = env_info.local_done                         # see if episode finished
+            agent_0.step(states, actions, rewards[0], next_states, done, 0) # agent 1 learns
+            agent_1.step(states, actions, rewards[1], next_states, done, 1) # agent 2 learns
+            scores += np.max(rewards)                                  # update the score for each agent
+            states = next_states                               # roll over states to next time step
 
-        for t in range(MAX_T):
-            # actions = agent.act(states, add_noise=ADD_NOISE)
-            action_0 = agent_0.act(states, ADD_NOISE)           # agent 0 chooses an action
-            action_1 = agent_1.act(states, ADD_NOISE)           # agent 1 chooses an action
-            actions = np.concatenate((action_0, action_1), axis=0)
-            actions = np.reshape(actions, (1, 4))
-            env_info = env.step(actions)[brain_name]            # send actions to environment
-            next_states = np.reshape(env_info.vector_observations, (1,48))   # get next state
-            rewards = env_info.rewards                          # get reward
-            dones = env_info.local_done                         # see if episode has finished
-            # save experience to replay buffer, perform learning step at defined interval
-#             for state, action, reward, next_state, done in zip(states, actions, rewards, next_states, dones):
-#                 agent.step(state, action, reward, next_state, done, t)
-            agent_0.step(states, actions, rewards[0], next_states, done, t, 0) # agent 0 learns
-            agent_1.step(states, actions, rewards[1], next_states, done, t, 1) # agent 1 learns
-            # agent.step(states, actions, rewards, next_states, dones, t)
-
-            states = next_states
-            scores += rewards
-            if np.any(dones):                                   # exit loop when episode ends
+            if np.any(done):                                  # exit loop if episode finished
                 break
 
-        duration = time.time() - start_time
-        min_scores.append(np.min(scores))             # save lowest score for a single agent
-        max_scores.append(np.max(scores))             # save highest score for a single agent
-        mean_scores.append(np.mean(scores))           # save mean score for the episode
-        scores_window.append(max_scores[-1])          # save mean score to window
-        moving_avgs.append(np.mean(scores_window))    # save moving average
+        scores_window.append(np.max(scores))
+        scores_all.append(np.max(scores))
+        moving_average.append(np.mean(scores_window))
 
         if i_episode % PRINT_EVERY == 0:
-            print('\rEpisode {} ({} sec)  -- \tMin: {:.1f}\tMax: {:.1f}\tMean: {:.1f}\tMov. Avg: {:.1f}'.format(\
-                  i_episode, round(duration), min_scores[-1], max_scores[-1], mean_scores[-1], moving_avgs[-1]))
+            print('Episodes {:0>4d}-{:0>4d}\tMax Reward: {:.3f}\tMoving Average: {:.3f}'.format(
+                i_episode-PRINT_EVERY, i_episode, np.max(scores_all[-PRINT_EVERY:]), np.mean(scores_window)))
 
-#         if train_mode and mean_scores[-1] > best_score:
-#             torch.save(agent.actor_local.state_dict(), actor_path)
-#             torch.save(agent.critic_local.state_dict(), critic_path)
-
-#         if moving_avgs[-1] >= solved_score and i_episode >= consec_episodes:
-#             print('\nEnvironment SOLVED in {} episodes!\tMoving Average ={:.1f} over last {} episodes'.format(\
-#                                     i_episode-consec_episodes, moving_avgs[-1], consec_episodes))
-#             if train_mode:
-#                 torch.save(agent.actor_local.state_dict(), actor_path)
-#                 torch.save(agent.critic_local.state_dict(), critic_path)
-#             break
-
+        if np.mean(scores_window)>=SOLVED_SCORE:
+            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.3f}'.format(
+                i_episode-CONSEC_EPISODES, np.mean(scores_window)))
+            torch.save(agent_0.actor_local.state_dict(), 'checkpoint_actor_0.pth')
+            torch.save(agent_0.critic_local.state_dict(), 'checkpoint_critic_0.pth')
+            torch.save(agent_1.actor_local.state_dict(), 'checkpoint_actor_1.pth')
+            torch.save(agent_1.critic_local.state_dict(), 'checkpoint_critic_1.pth')
+            break
 
     env.close()
-    # logger.close()
+
+
 
 if __name__=='__main__':
     main()
